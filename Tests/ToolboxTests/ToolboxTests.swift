125b1bf544d4cf7429465e2de62ba21f6cca1930
Jonas Zell <zell.jonas97@gmail.com>
2023-01-12 11:23:16 +0100

    merge branch 'dev' for 0.3 release

--- Tests/ToolboxTests/ToolboxTests.swift
+++ Tests/ToolboxTests/ToolboxTests.swift
@@ -89,7 +89,7 @@ final class ToolboxTests: XCTestCase {
     }
     
     func testStartOfDay() {
-        let calendar = Calendar(identifier: .gregorian)
+        let calendar = Calendar.reference
         let startComponents = DateComponents(timeZone: .current, year: 2020, month: 1, day: 1, hour: 12, minute: 0, second: 0)
         var currentDate = calendar.date(from: startComponents)!
         var expectedDay = 1
@@ -101,6 +101,59 @@ final class ToolboxTests: XCTestCase {
             XCTAssertEqual(components.year, expectedYear)
             XCTAssertEqual(components.month, expectedMonth)
             XCTAssertEqual(components.day, expectedDay)
+            
+            switch i {
+            case 31:
+                fallthrough
+            case 60:
+                fallthrough
+            case 91:
+                fallthrough
+            case 121:
+                fallthrough
+            case 152:
+                fallthrough
+            case 182:
+                fallthrough
+            case 213:
+                fallthrough
+            case 244:
+                fallthrough
+            case 274:
+                fallthrough
+            case 305:
+                fallthrough
+            case 335:
+                expectedDay = 1
+                expectedMonth += 1
+            case 366:
+                expectedDay = 1
+                expectedMonth = 1
+                expectedYear += 1
+            default:
+                expectedDay += 1
+            }
+            
+            currentDate.addTimeInterval(24*60*60)
+        }
+    }
+    
+    func testEndOfDay() {
+        let calendar = Calendar.reference
+        let startComponents = DateComponents(timeZone: .current, year: 2020, month: 1, day: 1, hour: 12, minute: 0, second: 0)
+        var currentDate = calendar.date(from: startComponents)!
+        var expectedDay = 1
+        var expectedMonth = 1
+        var expectedYear = 2020
+        
+        for i in 1...367 {
+            let components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: currentDate.endOfDay)
+            XCTAssertEqual(components.year, expectedYear)
+            XCTAssertEqual(components.month, expectedMonth)
+            XCTAssertEqual(components.day, expectedDay)
+            XCTAssertEqual(components.hour, 23)
+            XCTAssertEqual(components.minute, 59)
+            XCTAssertEqual(components.second, 59)
             
             switch i {
             case 31:
@@ -170,6 +223,215 @@ final class ToolboxTests: XCTestCase {
         for dateStr in dates {
             let date = dateFormatter.date(from: dateStr)!
             XCTAssertEqual(expected[i], dateFormatter.string(from: date.startOfMonth))
+            
+            i += 1
+        }
+    }
+    
+    func testEndOfMonth() {
+        let dates = [
+            "2021-02-28T00:00:00+0000",
+            "2021-02-28T10:00:00+0000",
+            "2021-02-01T18:59:59+0000",
+            "2021-02-01T00:00:00+0000",
+            
+            "2021-10-31T19:01:00+0000",
+            "2021-11-30T23:59:59+0000",
+            "2023-06-15T16:02:04+0000",
+        ]
+        
+        let expected = [
+            "2021-02-28T23:59:59+0000",
+            "2021-02-28T23:59:59+0000",
+            "2021-02-28T23:59:59+0000",
+            "2021-02-28T23:59:59+0000",
+            
+            "2021-10-31T23:59:59+0000",
+            "2021-11-30T23:59:59+0000",
+            "2023-06-30T23:59:59+0000",
+        ]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.timeZone = .utc
+        
+        var i = 0
+        for dateStr in dates {
+            let date = dateFormatter.date(from: dateStr)!
+            XCTAssertEqual(expected[i], dateFormatter.string(from: date.endOfMonth))
+            
+            i += 1
+        }
+    }
+    
+    func testStartOfWeekMonday() {
+        let dates = [
+            "2023-01-09T00:00:00+0000",
+            "2023-01-10T13:15:23+0000",
+            "2023-01-11T00:00:00+0000",
+            
+            "2023-01-08T00:00:00+0000",
+            "2023-01-07T00:00:00+0000",
+            "2023-01-06T00:00:00+0000",
+            "2023-01-05T00:00:00+0000",
+            "2023-01-04T00:00:00+0000",
+            "2023-01-03T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+        ]
+        
+        let expected = [
+            "2023-01-09T00:00:00+0000",
+            "2023-01-09T00:00:00+0000",
+            "2023-01-09T00:00:00+0000",
+            
+            "2023-01-02T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+        ]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.timeZone = .utc
+        
+        var i = 0
+        for dateStr in dates {
+            let date = dateFormatter.date(from: dateStr)!
+            XCTAssertEqual(expected[i], dateFormatter.string(from: date.startOfWeek(weekStartsOn: .monday)))
+            
+            i += 1
+        }
+    }
+    
+    func testEndOfWeekMonday() {
+        let dates = [
+            "2023-01-09T00:00:00+0000",
+            "2023-01-10T13:15:23+0000",
+            "2023-01-11T00:00:00+0000",
+            
+            "2023-01-08T00:00:00+0000",
+            "2023-01-07T00:00:00+0000",
+            "2023-01-06T00:00:00+0000",
+            "2023-01-05T00:00:00+0000",
+            "2023-01-04T00:00:00+0000",
+            "2023-01-03T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+        ]
+        
+        let expected = [
+            "2023-01-15T23:59:59+0000",
+            "2023-01-15T23:59:59+0000",
+            "2023-01-15T23:59:59+0000",
+            
+            "2023-01-08T23:59:59+0000",
+            "2023-01-08T23:59:59+0000",
+            "2023-01-08T23:59:59+0000",
+            "2023-01-08T23:59:59+0000",
+            "2023-01-08T23:59:59+0000",
+            "2023-01-08T23:59:59+0000",
+            "2023-01-08T23:59:59+0000",
+        ]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.timeZone = .utc
+        
+        var i = 0
+        for dateStr in dates {
+            let date = dateFormatter.date(from: dateStr)!
+            XCTAssertEqual(expected[i], dateFormatter.string(from: date.endOfWeek(weekStartsOn: .monday)))
+            
+            i += 1
+        }
+    }
+    
+    func testStartOfWeekSunday() {
+        let dates = [
+            "2023-01-09T00:00:00+0000",
+            "2023-01-10T13:15:23+0000",
+            "2023-01-11T00:00:00+0000",
+            
+            "2023-01-08T00:00:00+0000",
+            "2023-01-07T00:00:00+0000",
+            "2023-01-06T00:00:00+0000",
+            "2023-01-05T00:00:00+0000",
+            "2023-01-04T00:00:00+0000",
+            "2023-01-03T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+        ]
+        
+        let expected = [
+            "2023-01-08T00:00:00+0000",
+            "2023-01-08T00:00:00+0000",
+            "2023-01-08T00:00:00+0000",
+            
+            "2023-01-08T00:00:00+0000",
+            "2023-01-01T00:00:00+0000",
+            "2023-01-01T00:00:00+0000",
+            "2023-01-01T00:00:00+0000",
+            "2023-01-01T00:00:00+0000",
+            "2023-01-01T00:00:00+0000",
+            "2023-01-01T00:00:00+0000",
+        ]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.timeZone = .utc
+        
+        var i = 0
+        for dateStr in dates {
+            let date = dateFormatter.date(from: dateStr)!
+            XCTAssertEqual(expected[i], dateFormatter.string(from: date.startOfWeek(weekStartsOn: .sunday)))
+            
+            i += 1
+        }
+    }
+
+    func testEndOfWeekSunday() {
+        let dates = [
+            "2023-01-09T00:00:00+0000",
+            "2023-01-10T13:15:23+0000",
+            "2023-01-11T00:00:00+0000",
+            
+            "2023-01-08T00:00:00+0000",
+            "2023-01-07T00:00:00+0000",
+            "2023-01-06T00:00:00+0000",
+            "2023-01-05T00:00:00+0000",
+            "2023-01-04T00:00:00+0000",
+            "2023-01-03T00:00:00+0000",
+            "2023-01-02T00:00:00+0000",
+        ]
+        
+        let expected = [
+            "2023-01-14T23:59:59+0000",
+            "2023-01-14T23:59:59+0000",
+            "2023-01-14T23:59:59+0000",
+            
+            "2023-01-14T23:59:59+0000",
+            "2023-01-07T23:59:59+0000",
+            "2023-01-07T23:59:59+0000",
+            "2023-01-07T23:59:59+0000",
+            "2023-01-07T23:59:59+0000",
+            "2023-01-07T23:59:59+0000",
+            "2023-01-07T23:59:59+0000",
+        ]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.timeZone = .utc
+        
+        var i = 0
+        for dateStr in dates {
+            let date = dateFormatter.date(from: dateStr)!
+            XCTAssertEqual(expected[i], dateFormatter.string(from: date.endOfWeek(weekStartsOn: .sunday)))
             
             i += 1
         }
@@ -278,41 +540,32 @@ final class ToolboxTests: XCTestCase {
         XCTAssertEqual(dict["D"], [6, 7, 8, 9])
     }
     
-    func testRoundToMultipleInt() {
-        XCTAssertEqual(5, 5.roundedUp(toMultipleOf: 5))
-        XCTAssertEqual(5, 3.roundedUp(toMultipleOf: 5))
-        XCTAssertEqual(5, 1.roundedUp(toMultipleOf: 5))
-        XCTAssertEqual(5, 6.roundedDown(toMultipleOf: 5))
-        XCTAssertEqual(5, 9.roundedDown(toMultipleOf: 5))
+    func testArrayExtensions() {
+        // Unique
+        XCTAssertEqual([1, 2, 2, 3, 4, 4, 5].unique, [1, 2, 3, 4, 5])
+        XCTAssertEqual([2, 3, 2, 5, 6, 1, 6].unique, [2, 3, 5, 6, 1])
+        XCTAssertEqual([1, 1, 1, 1, 1, 1, 1].unique, [1])
         
-        XCTAssertEqual(0, 0.roundedDown(toMultipleOf: 5))
-        XCTAssertEqual(0, 0.roundedDown(toMultipleOf: 12134))
-        XCTAssertEqual(0, 0.roundedUp(toMultipleOf: 21893))
-        
-        XCTAssertEqual(56, 54.roundedUp(toMultipleOf: 7))
-        XCTAssertEqual(56, 60.roundedDown(toMultipleOf: 7))
-        
-        XCTAssertEqual(-5, (-1).roundedDown(toMultipleOf: 5))
-        XCTAssertEqual(-25, (-27).roundedUp(toMultipleOf: 5))
+        // Chunked
+        XCTAssertEqual([1, 2, 3, 4, 5, 6]   .chunked(into: 2              ), [[1, 2], [3, 4], [5, 6]])
+        XCTAssertEqual([1, 2, 3, 4, 5, 6, 7].chunked(into: 2              ), [[1, 2], [3, 4], [5, 6], [7]])
+        XCTAssertEqual([1, 2, 3, 4, 5, 6, 7].chunked(into: 2, padWith: 999), [[1, 2], [3, 4], [5, 6], [7, 999]])
     }
     
-    func testRoundToMultipleDouble() {
-        XCTAssertEqual(5.0, (4.7).roundedUp(toMultipleOf: 5))
-        XCTAssertEqual(5.0, 3.roundedUp(toMultipleOf: 5))
-        XCTAssertEqual(5.0, 1.roundedUp(toMultipleOf: 5))
-        XCTAssertEqual(5.0, (6.125).roundedDown(toMultipleOf: 5))
-        XCTAssertEqual(5.0, 9.roundedDown(toMultipleOf: 5))
+    func testStringExtensions() {
+        // Leftpad
+        XCTAssertEqual("hello".leftPadding(toExactLength: 10, withPad: " "), "     hello")
+        XCTAssertEqual("   hello".leftPadding(toExactLength: 10, withPad: "X"), "XX   hello")
         
-        XCTAssertEqual(0.0, 0.roundedDown(toMultipleOf: 5))
-        XCTAssertEqual(0.0, 0.roundedDown(toMultipleOf: 12134))
-        XCTAssertEqual(0.0, 0.roundedUp(toMultipleOf: 21893))
+        XCTAssertEqual("hello".leftPadding(toExactLength: 3, withPad: " "), "llo")
+        XCTAssertEqual("hello".leftPadding(toMinimumLength: 3, withPad: " "), "hello")
         
-        XCTAssertEqual(56.0, (55.9).roundedUp(toMultipleOf: 7))
-        XCTAssertEqual(56.0, 60.roundedDown(toMultipleOf: 7))
+        // Rightpad
+        XCTAssertEqual("hello".rightPadding(toExactLength: 10, withPad: " "), "hello     ")
+        XCTAssertEqual("   hello".rightPadding(toExactLength: 10, withPad: "X"), "   helloXX")
         
-        XCTAssertEqual(-5.0, (-1.5).roundedDown(toMultipleOf: 5))
-        XCTAssertEqual(-5.0, (-4.99).roundedDown(toMultipleOf: 5))
-        XCTAssertEqual(-25.0, (-27).roundedUp(toMultipleOf: 5))
-        XCTAssertEqual(-25.0, (-25.001).roundedUp(toMultipleOf: 5))
+        XCTAssertEqual("hello".rightPadding(toExactLength: 3, withPad: " "), "hel")
+        XCTAssertEqual("hello".rightPadding(toMinimumLength: 3, withPad: " "), "hello")
+        XCTAssertEqual("hello".rightPadding(toMinimumLength: 7, withPad: " "), "hello  ")
     }
 }
